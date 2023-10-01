f8554834dc482d8b1319e997ff3925dcfc2c270b
hanyue <47289292+hanyue1014@users.noreply.github.com>
2023-10-01 14:47:03 +0800

    Add walking target points

--- GP_Practical/main.cpp
+++ GP_Practical/main.cpp
@@ -130,18 +130,87 @@ namespace Robot
 		float newX, newY, newZ; // marks the newX and newY of the top side's sphere
 		float rootAngle = 90;
 		float jointAngle = 180;
-		const float upperLegLength = 5, botLegLength = 5;
+		const float upperArmLength = 5, botArmLength = 5;
 	public:
-		// creates the leg and draws it
+		// creates the hand at the position
 		Hand(Point3D p) : newX(p.x), newY(p.y), newZ(p.z) {}
 		
 		void draw()
 		{
 			cv.pushMatrix().translate(newX, newY, newZ);
 
-			cv.sphere({ 0, 0, 0, {255, 0, 0} }, 1);
+			cv
+				.pushMatrix()
+				.rotate(rootAngle, -1, 0, 0)
+				.sphere({ 0, 0, 0, {255, 0, 0} }, 1) // root bola
+				.cuboid({ -1, 1, -1, {255, 255, 0} }, { 1, -1, -(0 + upperArmLength) }) // 0 cuz wanna factor in the bola
+				.replotPrevBlocky3D(GL_LINE_LOOP, {0, 0, 0})
+				;
+
+			cv
+				.pushMatrix()
+				.translate(0, 0, -6)
+				.rotate(jointAngle, -1, 0, 0) // elbow bola
+				.sphere({ 0, 0, 0, {255, 0, 0} }, 1)
+				.cuboid({ -1, 1, 1, {255, 255, 0} }, { 1, -1, 0 + botArmLength }) // 0 cuz wanna factor in the bola as well
+				.replotPrevBlocky3D(GL_LINE_LOOP, {0, 0, 0})
+				.popMatrix()
+				;
+			cv.popMatrix(); // from root
 
 			cv.popMatrix();
+		}
+		
+		// only works if x = 0 after normalized
+		void solveIK(Point3D target)
+		{
+			// normalize target
+			float 
+				x = target.x - newX, 
+				y = target.y - newY, 
+				z = target.z - newZ;
+
+			Point3D v1 = { x, y, z }; // the root -> target vector
+			Point3D v2 = { 0, 0, -1 };  // the ori vector which z shouldn't actually matter
+
+			float magV1 = magnitude(v1);
+			float magV2 = magnitude(v2);
+
+			
+			float dotProd = dot(v1, v2);
+			float cosAlpha = dotProd / (magV1 * magV2);
+			float alpha;
+			if (magV1 * magV2 == 0)
+				alpha = 0;
+			else
+				alpha = acosf(cosAlpha);
+
+			rootAngle = alpha / PI * 180;
+			if (y > 0)
+				rootAngle *= -1;
+			// if longer than leg no need solve d, direct rotate 
+			if (magV1 > upperArmLength + botArmLength)
+			{
+				return;
+			}
+
+			float cosPhi = magV1 / 2;
+			float phi = acosf(cosPhi / upperArmLength);
+			// if magV1 is 0 then mean same as origin point, then should not rotate anything
+			if (magV1 == 0)
+			{
+				phi = 0;
+			}
+			phi = phi / PI * 180;
+			rootAngle += phi;
+
+			float theta = 180 - 90 - phi;
+			// if magV1 is 0 then mean same as origin point, then should not rotate anything
+			if (magV1 == 0)
+			{
+				theta = 0;
+			}
+			jointAngle = theta * 2;
 		}
 	};
 
@@ -154,7 +223,7 @@ namespace Robot
 		const float upperLegLength = 5, botLegLength = 5;
 
 	public:
-		// creates the leg and draws it
+		// creates the hand at position
 		Leg(Point3D p) : newX(p.x), newY(p.y), newZ(p.z) {}
 
 		void draw()
@@ -222,10 +291,61 @@ namespace Robot
 		}
 	};
 
+	// leftLeg would go upwards first
+	Point3D leftLegRestTarget = { -2, -13, 0 };
+	// TODO: tween these when walking
 	Point3D leftLegWalkTargets[] = {
-		{ 0, -11, 0 }, {  }
+		{ -2, -9, 0 }, // go upwards 
+		{ -2, -9, 1 }, // make a parabola when walking towards front
+		{ -2, -9, 2 },
+		{ -2, -10, 3 },
+		{ -2, -11, 4 },
+		{ -2, -12, 4 },
+		{ -2, -13, 4 }, // touched ground
+		{ -2, -13, 3 }, // start glide backwards
+		{ -2, -13, 2 },
+		{ -2, -13, 1 },
+		{ -2, -13, 0 },
+		{ -2, -13, -1 },
+		{ -2, -13, -2 },
+		{ -2, -13, -3 },
+		{ -2, -13, -4 }, // reached max backwards
+		{ -2, -12, -4 }, // start moving upwards in a parabola
+		{ -2, -11, -4 },
+		{ -2, -10, -3 },
+		{ -2, -9, -2 },
+		{ -2, -9, -1 } // next will be upwards center d (wrap around back to top)
 	};
-	Point3D leftLegCurrentTarget = leftLegWalkTargets[0];
+	Point3D leftLegCurrentTarget = leftLegRestTarget;
+
+	// rightLeg would go backwards first
+	Point3D rightLegRestTarget = { 2, -13, 0 };
+	Point3D rightLegWalkTargets[] = {
+		{ 2, -13, -1 }, // go backwards 
+		{ -2, -13, -2 }, // continue glide backwards
+		{ -2, -13, -3 },
+		{ -2, -13, -4 }, // reached max backwards
+		{ -2, -12, -4 }, // start moving upwards in a parabola
+		{ -2, -11, -4 },
+		{ -2, -10, -3 },
+		{ -2, -9, -2 },
+		{ -2, -9, -1 },
+		{ -2, -9, 0 }, // up center
+		{ -2, -9, 1 }, // make a parabola when walking towards front
+		{ -2, -9, 2 },
+		{ -2, -10, 3 },
+		{ -2, -11, 4 },
+		{ -2, -12, 4 },
+		{ -2, -13, 4 }, // touched ground
+		{ -2, -13, 3 }, // start glide backwards
+		{ -2, -13, 2 },
+		{ -2, -13, 1 },
+		{ -2, -13, 0 }, // next should wrap around
+	};
+	Point3D rightLegCurrentTarget = rightLegRestTarget;
+	// due to array arrangement only need one to keep track both
+	int bothLegWalkingIndex = 0;
+
 	void main()
 	{
 		// upper body
@@ -287,6 +407,9 @@ namespace Robot
 		Hand rightHand({5, 8, 0});
 		rightHand.draw();
 
+		Hand leftHand({ -5, 8, 0 });
+		leftHand.draw();
+
 		// body
 		cv
 			.cuboid({ -1, -1, 1, {0, 0, 255} }, { 1, -3, -1 })
@@ -308,8 +431,10 @@ namespace Robot
 			;
 
 		Leg leftLeg({2, -2, 0});
+		leftLeg.solveIK(leftLegCurrentTarget);
 		leftLeg.draw();
 		Leg rightLeg({-2, -2, 0});
+		rightLeg.solveIK(rightLegCurrentTarget);
 		rightLeg.draw();
 	}
 
@@ -317,11 +442,19 @@ namespace Robot
 	{
 		switch (key)
 		{
+		case 'W': // start walking, should move into main
+			int walkIndex = bothLegWalkingIndex++;
+			if (bothLegWalkingIndex >= sizeof(leftLegWalkTargets) / sizeof(leftLegWalkTargets[0]))
+				bothLegWalkingIndex = 0;
+			leftLegCurrentTarget = leftLegWalkTargets[walkIndex];
+			rightLegCurrentTarget = rightLegWalkTargets[walkIndex];
+			break;
 		}
 	}
 }
 
 ProjectionMode projectionMode = ORTHO;
+bool debug = true;
 Transform debugTrans;
 void display()
 {
@@ -333,8 +466,9 @@ void display()
 
 	// cv.rotate(0.01, 1, 1, 1);
 	//cv.cuboid({ 0, 0, 0, {255, 0, 0} }, { 5, 5, 5, {255, 255, 0}})
-	cv.showDebugGrid();
 	Robot::main();
+	if (debug)
+		cv.showDebugGrid();
 }
 
 // event handling
@@ -342,6 +476,9 @@ void handleKeyDownEvent(WPARAM key)
 {
 	switch (key)
 	{
+	case VK_F1:
+		debug = !debug;
+		return;
 	case 'P':
 		projectionMode = projectionMode == ORTHO ? PERSPECTIVE : ORTHO;
 		return; // alrd handle, no need proceed
