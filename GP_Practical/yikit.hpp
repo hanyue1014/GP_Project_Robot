a97eb873198312594a289b987f895e7aeadde5ba
yongyk <yikityong@gmail.com>
2023-10-02 16:00:02 +0800

    Add stuff

---
+++ GP_Practical/yikit.hpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#pragma comment (lib, "OpenGL32.lib")
+#pragma comment (lib, "GLU32.lib")
+
+namespace YiKit
+{
+	void weapon1()
+	{}
+}
